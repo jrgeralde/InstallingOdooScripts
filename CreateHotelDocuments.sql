6a2000da84419199512f1491c4182e4de88f5d33
John Roy Geralde <95401047+jrgeralde@users.noreply.github.com>
2026-04-06 11:22:26 +0800

    Create CreateHotelDocuments.sql

---
+++ CreateHotelDocuments.sql
@@ -0,0 +1,7 @@
+INSERT INTO hotel_documents (name, description)
+VALUES 
+    ('GRC', 'Guest Registration Card'),
+    ('RC', 'Room Charge'),
+    ('DM', 'Debit Memo'),
+    ('CM', 'Credit Memo'),
+    ('PR', 'Payment Receipt');
